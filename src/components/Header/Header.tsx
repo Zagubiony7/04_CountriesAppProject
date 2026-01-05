5ae7f67b0a0b14e8fec61133a6500089d71b6c2e
Zagubiony7 <jakubkocemba1@onet.pl>
2026-01-05 20:40:47 +0100

    Remove change theme button

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,14 +1,14 @@
 import "./Header.css";
-import moon_icon from "../../assets/moon.png";
+// import moon_icon from "../../assets/moon.png";
 
 const Header = () => {
   return (
     <header className="header">
       <h1>Where in the World?</h1>
-      <button className="switch-theme">
+      {/* <button className="switch-theme">
         <img src={moon_icon} alt="search loop image" />
         Dark Mode
-      </button>
+      </button> */}
     </header>
   );
 };
